e969f38330fc4eb1193c7f95f79b02b674da6723
Mitchell Porter <mitchellporter@gmail.com>
2017-02-23 14:47:59 -0800

    item model

---
+++ app/items/itemModel.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var statuses = ['in_progress', 'completed'];
+
+var ItemSchema = new Schema({
+	created_at: {
+		type: Date,
+		required: true
+	},
+	updated_at: {
+		type: Date,
+		required: true
+	},
+	task: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+        required: true
+    },
+    status: {
+		type: String,
+		required: true,
+		default: 'in_progress',
+		enum: statuses
+	}
+});
+
+ItemSchema.pre('validate', function(next) {
+	if(!this.created_at) this.created_at = new Date();
+	this.updated_at = new Date();
+	next();
+});
+
+ItemSchema.methods = {
+	toJSON: function() {
+		var obj = this.toObject();
+		obj.created_at = this.created_at.getTime();
+		obj.updated_at = this.updated_at.getTime();
+		if (obj.due_date) obj.due_date = obj.due_date.getTime();
+		delete obj.__v;
+		return obj;
+	}
+}
+
+module.exports = mongoose.model('Item', ItemSchema);
