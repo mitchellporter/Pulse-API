fe74bd1143998de5a3c04457320675708f3a604e
Mitchell Porter <mitchellporter@gmail.com>
2017-02-23 14:50:12 -0800

    api code

---
+++ app/api/api.js
@@ -0,0 +1,7 @@
+var router = require('express').Router();
+
+router.use('/users', require('../users/userRoutes'));
+router.use('/tasks', require('../tasks/taskRoutes'));
+router.use('/items', require('../items/itemRoutes'));
+
+module.exports = router;
