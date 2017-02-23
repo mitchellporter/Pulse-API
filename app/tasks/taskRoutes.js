6bf599e41680455ecc5f27da3832232d6ab8e18a
Mitchell Porter <mitchellporter@gmail.com>
2017-02-23 14:59:34 -0800

    task routes file

---
+++ app/tasks/taskRoutes.js
@@ -0,0 +1,11 @@
+var router = require('express').Router();
+var taskController = require('./taskController');
+var auth = require('../auth/auth');
+
+
+router.param('id',taskController.params);
+
+router.route('/')
+.get(taskController.get)
+
+module.exports = router;
