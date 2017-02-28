8dfbbbf2aa1aabb1509ff9f0596351df05944e8d
Mitchell Porter <mitchellporter@gmail.com>
2017-02-28 13:57:58 -0800

    todo

--- app/update_responses/updateResponseController.js
+++ app/update_responses/updateResponseController.js
@@ -2,6 +2,9 @@ var logger = require('../../lib/logger');
 var UpdateResponse = require('./updateResponseModel');
 var async = require('async');
 
+// TODO: The problem with this is after we create an UpdateResponse,
+// we need to update the completion_percentage field on the task, but because
+// we havent nested this route, we can't use req.task like normal. Revisit this later.
 exports.post = function(req, res, next) {
     var sender = req.user;
 
