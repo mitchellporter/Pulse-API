873c37ef2674c82bcc39eff7627d402bc345e9ee
Mitchell Porter <mitchellporter@gmail.com>
2017-03-08 14:33:55 -0800

    populating task invitations task field for assigner

--- app/tasks/taskController.js
+++ app/tasks/taskController.js
@@ -254,7 +254,7 @@ exports.tasksCreated = function (req, res, next) {
 
 	function findTaskInvitationsSentByUser(callback) {
 		TaskInvitation.find({ sender: user, status: 'pending' })
-			.populate([{ path: 'sender' }, { path: 'receiver' }, { path: 'task', populate: [{ path: 'items' }, { path: 'assignees', select: '_id name email position avatar_url' }] }])
+			.populate([{ path: 'sender' }, { path: 'receiver' }, { path: 'task', populate: [{ path: 'items' }, { path: 'assignees', select: '_id name email position avatar_url' }, { path: 'assigner', select: '_id name email position avatar_url' }] }])
 			.then(function (task_invitations) {
 				logger.silly('found this many task invitations: ' + task_invitations.length);
 				response.task_invitations = task_invitations;
