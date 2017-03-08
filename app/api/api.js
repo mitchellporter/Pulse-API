10c55f345fd7efd7f51db923c6aa1d153db95e79
Mitchell Porter <mitchellporter@gmail.com>
2017-03-07 16:48:02 -0800

    updated task invitations routes

--- app/api/api.js
+++ app/api/api.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 
 router.use('/users', require('../users/userRoutes'));
 router.use('/tasks', require('../tasks/taskRoutes'));
+router.use('/task_invitations', require('../tasks/taskInvitationRoutes'));
 router.use('/update_requests', require('../update_requests/updateRequestRoutes'));
 router.use('/teams', require('../teams/teamRoutes'));
 router.use('/feeds', require('../Feed/myTaskRoutes'));
