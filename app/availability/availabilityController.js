1ebd85a3c17cafc2fb2466166d9d19e0f94fa36a
Mitchell Porter <mitchellporter@gmail.com>
2017-03-22 14:06:09 -0700

    availability endpoints for usernames and emails

--- app/availability/availabilityController.js
+++ app/availability/availabilityController.js
@@ -1,5 +1,6 @@
 var logger = require('../../lib/logger');
 var Team = require('../teams/teamModel');
+var User = require('../users/userModel');
 
 exports.teams = function(req, res, next) {
     var team_name = req.query.name;
@@ -23,6 +24,62 @@ exports.teams = function(req, res, next) {
         res.status(200).json({
             success: false,
             error: 'team name not available'
+        });
+    }
+};
+
+exports.usernames = function(req, res, next) {
+    var team = req.team;
+    var username = req.query.username;
+
+    User.findOne({ team: team, username: username })
+    .then(function(user) {
+        logger.silly('found user: ' + user);
+        if (!user) return usernameIsAvailable();
+        usernameAlreadyTaken();
+    })
+    .catch(next);
+
+    // TODO: Always a successful query regardless of username availability?
+    function usernameIsAvailable() {
+        res.status(200).json({
+            success: true,
+            username: username
+        });
+    }
+
+    function usernameAlreadyTaken() {
+        res.status(200).json({
+            success: false,
+            error: 'someone on this team already has that username'
+        });
+    }
+};
+
+exports.emails = function(req, res, next) {
+    var team = req.team;
+    var email_address = req.query.email_address;
+
+    User.findOne({ team: team, email_address: email_address })
+    .then(function(user) {
+        logger.silly('found user: ' + user);
+        if (!user) return emailAddressIsAvailable();
+        emailAddressAlreadyTaken();
+    })
+    .catch(next);
+
+    // TODO: Always a successful query regardless of username availability?
+    function emailAddressIsAvailable() {
+        res.status(200).json({
+            success: true,
+            email_address: email_address
+        });
+    }
+
+    function emailAddressAlreadyTaken() {
+        res.status(200).json({
+            success: false,
+            error: 'someone on this team already has that email address'
         });
     }
 };
