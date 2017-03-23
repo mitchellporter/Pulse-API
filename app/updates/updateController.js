d61f2d155899aeee897dc107fdbdbd69012bf933
Mitchell Porter <mitchellporter@gmail.com>
2017-03-23 10:14:07 -0700

    cleanup

--- app/updates/updateController.js
+++ app/updates/updateController.js
@@ -27,68 +27,6 @@ exports.get = function(req, res, next) {
     .catch(next);
 };
 
-exports.respondToUpdate = function (req, res, next) {
-    var update_request = req.update_request;
-    var sender = req.user;
-    var receiver = update_request.sender;
-    
-    update_request.populate([{ path: 'task'}, { path: 'sender' }]).execPopulate()
-        .then(function (update_request) {
-            var task = update_request.task;
-
-            var update = new Update(req.body);
-            update.task = task;
-            update.sender = sender;
-            update.receiver = receiver;
-
-            update.save()
-            .then(function(update) {
-
-                update_request.status = 'responded';
-                update_request.isNew = false;
-                return update_request.save();
-            })
-            .then(function (update_request) {
-                task.updates.push(update);
-                task.completion_percentage = update.completion_percentage;
-                task.isNew = false;
-                return task.save();
-
-                logger.silly('saved update_request: ' + update_request);
-
-                sendMessage();
-
-            })
-            .then(function(task) {
-                res.status(201).json({
-                        success: true,
-                        update: update,
-                        update_request: update_request
-                });
-            })
-            .catch(next);
-        })
-        .catch(next);
-
-    function sendMessage() {
-        logger.silly('about to send response to update request notification!!!');
-        var channel = update.receiver;
-        var message = {
-            type: 'update',
-            update: update
-        }
-
-        messenger.sendMessage(channel, message)
-            .then(function (response) {
-                logger.silly('successfully sent response to update request notification!');
-                logger.silly('response: ' + response);
-            })
-            .catch(function (err) {
-                logger.silly('error: ' + err);
-            })
-    }
-};
-
 exports.put = function(req, res, next) {
     logger.silly('updates put');
 
