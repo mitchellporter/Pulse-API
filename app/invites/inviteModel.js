4a7f777d3f7577c66ef20929ea1ded3b98f760c3
Mitchell Porter <mitchellporter@gmail.com>
2017-03-28 11:54:24 -0700

    send rough draft

--- app/invites/inviteModel.js
+++ app/invites/inviteModel.js
@@ -60,7 +60,39 @@ function toJSON() {
 }
 
 function send() {
-    
+    return new Promise(function (resolve, reject) {
+        var options = {
+            service: 'Gmail',
+            auth: {
+                user: 'ellroiapp@gmail.com',
+                pass: 'kirkland1234'
+            }
+        };
+
+        var callback = function (err, success) {
+            if (err) console.log(err);
+            if (success) console.log(success);
+            ready();
+        }
+
+        var Emailer = require('./emailer');
+        var emailer = new Emailer(options, callback);
+
+        function ready() {
+            logger.silly('about to send email to invitee email address: ' + this.email);
+            var message = {
+                from: 'ellroiapp@gmail.com',
+                to: this.email,
+                subject: 'You have been invited to a task!',
+                text: 'You have been invited to a task!',
+                html: '<p>You have been invited to a task!</p>'
+            };
+
+            emailer.send(message)
+            .then(resolve)
+            .catch(reject);
+        }
+    }.bind(this));
 }
 
 
