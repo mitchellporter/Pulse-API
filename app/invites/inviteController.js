7000623a664c6db3f11bf0185c5c96e521cd56d6
Mitchell Porter <mitchellporter@gmail.com>
2017-03-28 09:43:32 -0700

    parsing invite type

--- app/invites/inviteController.js
+++ app/invites/inviteController.js
@@ -7,12 +7,18 @@ var Invite = require('./inviteModel');
 exports.post = function(req, res, next) {
     var team = req.team; // optional
     var task = req.task; // optional
+
+    var type;
+    if (team) type = 'team';
+    if (task) type = 'task';
+
     var invitees = req.body.invitees;
 
     var invites = [];
     async.forEachOf(invitees, function(value, key, callback) {
         var invitee = value;
         var invite = new Invite({
+            type: type,
             name: invitee.name,
             email: invitee.email,
             team: team,
