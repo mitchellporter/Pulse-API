19ccc3bc1a4a1107e714fa185a8b5842c5dcbecb
Mitchell Porter <mitchellporter@gmail.com>
2017-03-28 09:31:16 -0700

    validation hook + toJSON

--- app/invites/inviteModel.js
+++ app/invites/inviteModel.js
@@ -30,6 +30,21 @@ var InviteSchema = new Schema({
     }
 });
 
+InviteSchema.pre('validate', function(next) {
+    if(!this.created_at) this.created_at = new Date();
+	this.updated_at = new Date();
+	next();
+});
+
+InviteSchema.methods = {
+    toJSON: toJSON
+}
+
+function toJSON() {
+    var obj = this.toObject();
+	delete obj.__v;
+	return obj;
+}
 
 
 module.exports = mongoose.model('Invite', InviteSchema);
