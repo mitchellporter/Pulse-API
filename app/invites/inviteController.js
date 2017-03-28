18517698be917453cb803df60ada93da2fe116b0
Mitchell Porter <mitchellporter@gmail.com>
2017-03-28 09:39:57 -0700

    invite controller post

---
+++ app/invites/inviteController.js
@@ -0,0 +1,35 @@
+var logger = require('../../lib/logger');
+var async = require('async');
+var _ = require('lodash');
+var Invite = require('./inviteModel');
+
+
+exports.post = function(req, res, next) {
+    var team = req.team; // optional
+    var task = req.task; // optional
+    var invitees = req.body.invitees;
+
+    var invites = [];
+    async.forEachOf(invitees, function(value, key, callback) {
+        var invitee = value;
+        var invite = new Invite({
+            name: invitee.name,
+            email: invitee.email,
+            team: team,
+            task: task
+        });
+        invites.push(invite);
+        callback();
+    }, function(err) {
+        if (err) return next(err);
+        
+        Invite.create(invites)
+        .then(function(invites) {
+            res.status(201).json({
+                success: true,
+                invites: invites
+            });
+        })
+        .catch(next);
+    });
+};
