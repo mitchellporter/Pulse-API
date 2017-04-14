13d5b0380bd3feab97ed89ab22d6f155f9025b02
Mitchell Porter <mitchellporter@gmail.com>
2017-04-14 12:28:48 -0700

    modular middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const config = require('./config/config');
 const logger = require('./lib/logger');
+const middleware = require('./lib/middleware');
 const errors = require('./lib/errors');
 const throng = require('throng');
 const Promise = require('bluebird');
@@ -9,7 +10,6 @@ const mongoose = require('mongoose').connect(config.mongo_url);
 mongoose.Promise = Promise;
 
 const api = require('./app/api/api');
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
@@ -23,32 +23,10 @@ throng(options, start);
 function start() {
 	
 	app.set('x-powered-by', false);
-
-	// TODO: Move this to its own module
-	// source: http://stackoverflow.com/a/23816083/3344977
-	function wwwRedirect(req, res, next) {
-		if (req.headers.host.slice(0, 4) === 'www.') {
-			var newHost = req.headers.host.slice(4);
-			return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-		}
-		next();
-	};
-
 	app.set('trust proxy', true);
-	app.use(wwwRedirect);
-
-	// source: http://stackoverflow.com/a/36316721/3344977
-	app.get('*', function (req, res, next) {
-		if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-			res.redirect('https://' + req.hostname + req.url)
-		else
-			next() /* Continue to other routes if we're not redirecting */
-	});
 
 	// middleware
-	app.use(require('morgan')('dev'));
-	app.use(bodyParser.urlencoded({extended: true}));
-	app.use(bodyParser.json());
+	middleware(app);
 	
 	// routes
 	app.use('/api/' + config.api_version, api);
