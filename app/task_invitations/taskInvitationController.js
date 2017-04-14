e531932f4c170c8b7da6e6454f0047d3c12e9768
Mitchell Porter <mitchellporter@gmail.com>
2017-04-14 13:19:09 -0700

    arrow functions for task invitation controller

--- app/task_invitations/taskInvitationController.js
+++ app/task_invitations/taskInvitationController.js
@@ -5,12 +5,12 @@ var async = require('async');
 exports.params = function(req, res, next, id) {
     TaskInvitation.findById(id)
     .populate([{ path: 'task', populate: [{ path: 'assigner' }, { path: 'assignees' }, { path: 'items' }] }])
-	.then(function(task_invitation) {
+	.then((task_invitation) => {
 		if(!task_invitation) return next(new Error('no task invitation exists with that id'));
 		req.task_invitation = task_invitation;
 		next();
 	})
-	.catch(function(err) {
+	.catch((err) => {
 		next(err);
 	})
 };
@@ -31,16 +31,16 @@ exports.put = function(req, res, next) {
     
     task_invitation.isNew = false;
     task_invitation.save()
-    .then(function(task_invitation) {
+    .then((task_invitation) => {
 
         var task = task_invitation.task;
         task.status = 'in_progress';
         task.isNew = false;
         task.save()
-            .then(function (task) {
+            .then((task) => {
                 logger.silly('saved task status: ' + task.status);
                 task_invitation.populate([{ path: 'sender', select: '_id name email position avatar_url' }, { path: 'receiver', select: '_id name email position avatar_url' }]).execPopulate()
-                    .then(function (task_invitation) {
+                    .then((task_invitation) => {
                         res.status(201).json({
                             success: true,
                             task_invitation: task_invitation
