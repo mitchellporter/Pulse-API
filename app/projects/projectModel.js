551ee3533894050750f3506b3571d8356ca0a7fc
Mitchell Porter <mitchellporter@gmail.com>
2017-04-21 12:59:36 -0700

    project model

---
+++ app/projects/projectModel.js
@@ -0,0 +1,58 @@
+const Schema = require('mongoose').Schema;
+
+var ProjectSchema = new Schema({
+	created_at: {
+		type: Date,
+		required: true
+	},
+	updated_at: {
+		type: Date,
+		required: true
+	},
+    name: {
+        type: String,
+        required: true
+    },
+    due_date: {
+        type: Date
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+    }],
+    completion_percentage: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    standups_count: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    tasks_in_progress_count: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    completed_tasks_count: {
+        type: Number,
+        required: true,
+        default: 0
+    }    
+});
+
+ProjectSchema.pre('validate', function(next){
+	if(!this.created_at) this.created_at = new Date();
+	this.updated_at = new Date();
+
+	next();
+});
+
+// TODO: Try using hard bind and making this work
+ProjectSchema.pre('save', function(next) {
+
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
