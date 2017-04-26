f98f53d5c2163a0b2f48df6e641939e31719781d
Mitchell Porter <mitchellporter@gmail.com>
2017-04-26 14:23:08 -0700

    new standup model

--- app/standups/standupModel.js
+++ app/standups/standupModel.js
@@ -1,39 +1,21 @@
-const logger = require('../../lib/logger');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Model = require('objection').Model;
+const User = require('../users/user');
 
-var StandupSchema = new Schema({
-	created_at: {
-		type: Date,
-		required: true
-	},
-	updated_at: {
-		type: Date,
-		required: true
-	},
-    text: {
-        type: String,
-        required: true
-    },
-	author: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
+class Standup extends Model {
+	static get tableName() {
+		return 'Standup';
 	}
-});
 
-StandupSchema.pre('validate', function(next) {
-	if(!this.created_at) this.created_at = new Date();
-	this.updated_at = new Date();
-	next();
-});
-
-StandupSchema.methods = {
-	toJSON: function() {
-		var obj = this.toObject();
-		delete obj.__v;
-		return obj;
+	static get relationMapping() {
+		return {
+			author: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'standup.author',
+					to: 'user.id'
+				}
+			}
+		}
 	}
 }
-
-module.exports = mongoose.model('Standup', StandupSchema);
