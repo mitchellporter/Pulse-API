adc97e5ab346ffb02b535bad72b105f204159ba9
Mitchell Porter <mitchellporter@gmail.com>
2017-04-26 14:34:03 -0700

    new update model

--- app/updates/updateModel.js
+++ app/updates/updateModel.js
@@ -1,61 +1,47 @@
-const async = require('async');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const logger = require('../../lib/logger');
-const Task = require('../tasks/taskModel');
+const Model = require('objection').Model;
+const User = require('../users/user');
+const Task = require('../tasks/task');
+const UpdateRequest = require('../update_requests/updateRequest');
 
-const types = ['automated', 'requested', 'random'];
-
-var UpdateSchema = new Schema({
-    created_at: {
-        type: Date,
-        required: true
-    },
-    updated_at: {
-        type: Date,
-        required: true
-    },
-	comment: {
-		type: String
-	},
-	sender: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	receiver: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	task: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Task',
-		required: true
-	},
-	update_request: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'UpdateRequest'
+class Update extends Model {
+	static get tableName() {
+		return 'Update';
 	}
-});
 
-UpdateSchema.pre('validate', function(next) {
-	if(!this.created_at) this.created_at = new Date();
-	this.updated_at = new Date();
-	next();
-});
-
-UpdateSchema.pre('save', function(next) {
-	if (!this.isNew) return next();
-	next();
-});
-
-UpdateSchema.methods = {
-	toJSON: function() {
-		var obj = this.toObject();
-		delete obj.__v;
-		return obj;
+	static get relationMappings() {
+		return {
+			sender: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'update.sender',
+					to: 'user.id'
+				}
+			},
+			receiver: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'update.receiver',
+					to: 'user.id'
+				}
+			},
+			task: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Task,
+				join: {
+					from: 'update.task',
+					to: 'task.id'
+				}
+			},
+			update_request: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: UpdateRequest,
+				join: {
+					from: 'update.update_request',
+					to: 'updaterequest.id'
+				}
+			}
+		}
 	}
-};
-
-module.exports = mongoose.model('Update', UpdateSchema);
+}
