96c292364e4bc54f683e576899df12dd96e5d53d
Mitchell Porter <mitchellporter@gmail.com>
2017-04-26 14:19:38 -0700

    new subtask model

--- app/subtasks/subtaskModel.js
+++ app/subtasks/subtaskModel.js
@@ -1,51 +1,38 @@
-const logger = require('../../lib/logger');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Model = require('objection').Model;
+const Task = require('../tasks/task');
+const User = require('../users/user');
 
-const statuses = ['in_progress', 'completed'];
-
-var SubtaskSchema = new Schema({
-	created_at: {
-		type: Date,
-		required: true
-	},
-	updated_at: {
-		type: Date,
-		required: true
-	},
-	task: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Task',
-		required: true 
-	},
-	completed_by: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-	},
-    text: {
-        type: String,
-        required: true
-    },
-    status: {
-		type: String,
-		required: true,
-		default: 'in_progress',
-		enum: statuses
+class Subtask extends Model {
+	static get tableName() {
+		return 'Subtask';
 	}
-});
 
-SubtaskSchema.pre('validate', function(next) {
-	if(!this.created_at) this.created_at = new Date();
-	this.updated_at = new Date();
-	next();
-});
-
-SubtaskSchema.methods = {
-	toJSON: function() {
-		var obj = this.toObject();
-		delete obj.__v;
-		return obj;
+	static get relationMappings() {
+		return {
+			task: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Task,
+				join: {
+					from: 'subtask.task',
+					to: 'task.id'
+				}
+			},
+			created_by: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'subtask.created_by',
+					to: 'user.id'
+				}
+			},
+			completed_by: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'subtask.completed_by',
+					to: 'user.id'
+				}
+			}
+		}
 	}
-};
-
-module.exports = mongoose.model('SubTask', SubtaskSchema);
+}
