1f8601c911417655a19420aa162e4c42cd945a41
Mitchell Porter <mitchellporter@gmail.com>
2017-04-27 13:05:40 -0700

    new task invitation model

--- app/task_invitations/taskInvitation.js
+++ app/task_invitations/taskInvitation.js
@@ -1,85 +1,40 @@
-const logger = require('../../lib/logger');
-const async = require('async');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Model = require('objection').Model;
+const User = require('../users/user');
+const Task = require('../tasks/task');
 
-const statuses = ['pending', 'accepted', 'denied'];
-const update_days = ['monday', 'wednesday', 'friday'];
-
-var TaskInvitationSchema = new Schema({
-	created_at: {
-		type: Date,
-		required: true
-	},
-	updated_at: {
-		type: Date,
-		required: true
-	},
-	sender: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-    receiver: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-		// required: true
-	},
-	task: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Task',
-		required: true
-	},
-	status: {
-		type: String,
-		required: true,
-		default: 'pending',
-		enum: statuses
+class TaskInvitation extends Model {
+	static get tableName() {
+		return 'TaskInvitation';
 	}
-});
 
-TaskInvitationSchema.pre('validate', function(next) {
-	if(!this.created_at) this.created_at = new Date();
-	this.updated_at = new Date();
-	next();
-});
-
-TaskInvitationSchema.statics = {
-	createTaskInvitationsForAssignees
-}
-
-TaskInvitationSchema.methods = {
-	toJSON: function() {
-		var obj = this.toObject();
-		delete obj.__v;
-		return obj;
+	static get relationMappings() {
+		return {
+			sender: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'taskinvitation.sender',
+					to: 'user.id'
+				}
+			},
+			receiver: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: User,
+				join: {
+					from: 'taskinvitation.receiver',
+					to: 'user.id'
+				}
+			},
+			task: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Task,
+				join: {
+					from: 'taskinvitation.task',
+					to: 'task.id'
+				}
+			}
+		}
 	}
-}
+};
 
-function createTaskInvitationsForAssignees(task, assignees) {
-
-	logger.silly('creating task invitiations for assignees');
-
-	return new Promise((resolve, reject) => {
-			var task_invitations = [];
-			async.eachOf(assignees, (value, key, callback) => {
-				var assignee = value;
-				var task_invitation = new this({
-					sender: task.assigner,
-					receiver: assignee,
-					task: task,
-				});
-				task_invitations.push(task_invitation);
-				callback();
-			}, (err) => {
-				if (err) return reject(err);
-				
-				this.create(task_invitations)
-				.then(resolve)
-				.catch(reject);
-
-			});
-		});
-}
-
-module.exports = mongoose.model('TaskInvitation', TaskInvitationSchema);
+module.exports = TaskInvitation;
